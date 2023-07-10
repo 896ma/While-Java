4beac5750c524070abf05b9cd22ce931dfaab89a
896ma <solarcrash3@gmail.com>
2023-07-10 14:35:04 +0300

    Even java

---
+++ whileEven.java
@@ -0,0 +1,20 @@
+public class whileEven {
+
+    public static void main (String args[]){
+
+
+        int num=1;
+    
+        while(  num<=100){
+            num++;
+
+             if(num %2==0)
+            System.out.println(num);
+            
+        }
+        
+    }
+
+    }
+   
+
